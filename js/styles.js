d9c867d66bc96623041e23f301c2793762324256
rajvi16-rarepixels <rajvi.rarepixelsdesign@gmail.com>
2026-06-04 15:32:42 +0530

    fix 🐛: added custom swiper in testimonial section instead of swiper.js

--- js/styles.js
+++ js/styles.js
@@ -1,20 +1,20 @@
-const testimonialSwiper = new Swiper('.testimonial-swiper', {
-  loop: true,
-  // slidesPerView: 3,
-  spaceBetween: 20,
-  autoPlay: true,
-  delay: 1000,
+// const testimonialSwiper = new Swiper('.testimonial-swiper', {
+//   loop: true,
+//   // slidesPerView: 3,
+//   spaceBetween: 20,
+//   autoPlay: true,
+//   delay: 1000,
 
-  navigation: {
-    nextEl: '.testimonial-swiper-right',
-    prevEl: '.testimonial-swiper-left',
-  },
+//   navigation: {
+//     nextEl: '.testimonial-swiper-right',
+//     prevEl: '.testimonial-swiper-left',
+//   },
 
-  breakpoints: {
-    768: { slidesPerView: 2, spaceBetween: 20 },
-    1200: { slidesPerView: 3 }
-  }
-});
+//   breakpoints: {
+//     768: { slidesPerView: 2, spaceBetween: 20 },
+//     1200: { slidesPerView: 3 }
+//   }
+// });
 
 
 const tabs = document.querySelectorAll(".tab");
@@ -58,16 +58,16 @@ prevBtn.addEventListener("click", () => {
   updateSwiper();
 });
 
-const mentorSwiper = new Swiper('.mentor-swiper', {
-  loop: true,
-  // slidesPerView: 3,
-  spaceBetween: 20,
-  delay: 1000,
-  breakpoints: {
-    768: { slidesPerView: 2 },
-    1360: { slidesPerView: 3 }
-  }
-});
+// const mentorSwiper = new Swiper('.mentor-swiper', {
+//   loop: true,
+//   // slidesPerView: 3,
+//   spaceBetween: 20,
+//   delay: 1000,
+//   breakpoints: {
+//     768: { slidesPerView: 2 },
+//     1360: { slidesPerView: 3 }
+//   }
+// });
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
@@ -79,19 +79,89 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle(('active'));
 })
 
-const whatweSwiper = new Swiper('.what-we-offer-swiper', {
-  loop: true,
-  // slidesPerView: 3,
-  spaceBetween: 20,
-  autoPlay: true,
-  delay: 1000,
+// const whatweSwiper = new Swiper('.what-we-offer-swiper', {
+//   loop: true,
+//   // slidesPerView: 3,
+//   spaceBetween: 20,
+//   autoPlay: true,
+//   delay: 1000,
 
-  breakpoints :{
-    0: { slidesPerView: 1 },
-  },
+//   breakpoints :{
+//     0: { slidesPerView: 1 },
+//   },
 
-  navigation: {
-    nextEl: '.what-we-offer-swiper-right',
-    prevEl: '.what-we-offer-swiper-left',
-  },
+//   navigation: {
+//     nextEl: '.what-we-offer-swiper-right',
+//     prevEl: '.what-we-offer-swiper-left',
+//   },
+// });
+
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".testimonial-arrows");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, startX, startScrollLeft, timeoutId;
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+  carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+})
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+})
+
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  })
 });
+
+const dragStart = () => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+}
+
+const autoPlay = () => {
+  if(window.innerWidth < 800) return;
+  timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
+const infiniteScroll = () => {
+  if(carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    coursel.scrollLeft = coursel.scrollWidth - ( 2 * carousel.offsetWidth );
+    carousel.classList.remove("no-transition");
+  }
+  else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  clearTimeout(timeoutId);
+  if(!wrapper.matches(":hover")) autoPlay();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
